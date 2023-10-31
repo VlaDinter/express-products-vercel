36056cec4bb40295e29793d536e8ebe736681631
VlaDinter <vladyslavdinter@gmail.com>
2023-10-31 09:55:03 +0200

    add mongo url

---
+++ src/repositories/db.ts
@@ -0,0 +1,25 @@
+import { MongoClient, ObjectId } from 'mongodb';
+
+export type ProductType = {
+    title: string,
+    _id?: ObjectId
+};
+
+// Connection URL
+const url = 'mongodb+srv://admin:admin@cluster0.xnbydka.mongodb.net/shop-dev?retryWrites=true&w=majority';
+
+console.log('url', url);
+
+const client = new MongoClient(url);
+
+export const productCollection = client.db().collection<ProductType>('products');
+export const runDb = async () => {
+    try {
+        await client.connect();
+        console.log('Connected successfully to server');
+    } catch (e) {
+        console.log('Don\'t connected successfully to server');
+        throw e;
+        await client.close();
+    }
+};
